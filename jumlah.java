e65d77d6aefefe2b160e6cba601978f2bc3a62f7
Abdur097 <74463635+Abdur097@users.noreply.github.com>
2021-11-09 16:03:56 +0700

    Add files via upload

---
+++ jumlah.java
@@ -0,0 +1,17 @@
+/**
+ * jumlah
+ */
+public class jumlah {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        int hasil;
+        hasil = a + b;
+
+        System.out.println("Nilai dari A =  " + a);
+        System.out.println("Nilai dari B =  " + b);
+        System.out.println("Maka hasil dari penjumlahan adalah =  " + hasil);
+
+    }
+}
